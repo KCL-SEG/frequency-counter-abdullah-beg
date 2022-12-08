29bae97c1cdb6ddb159add4e030a366ba1dd66dc
Abdullah Beg <abdullahbeg@outlook.com>
2022-12-08 19:45:27 +0000

    Solution

--- frequencies.py
+++ frequencies.py
@@ -3,5 +3,13 @@
 
 def frequencies(items):
     frequencies = {}
-    # Your code goes here
+    
+    for item in items:
+        item = str(item)
+        if item in frequencies.keys():
+            frequencies[item] = frequencies[item] + 1
+        else:
+            print(f'{item}')
+            frequencies.update({item : 1})
+
     return frequencies
